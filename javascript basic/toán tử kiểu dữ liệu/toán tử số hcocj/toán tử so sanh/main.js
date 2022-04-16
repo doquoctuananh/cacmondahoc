a537d651e9906cda56bd394bf414cf2676a183fe
doquoctuananh <71720140+doquoctuananh@users.noreply.github.com>
2022-04-16 10:32:09 +0700

    upload

---
+++ javascript basic/toán tử kiểu dữ liệu/toán tử số hcocj/toán tử so sanh/main.js
@@ -0,0 +1,16 @@
+/**
+ * toán tử so sánh
+ * ===
+ * !==
+ */
+
+// truthy và falsy 
+
+var a='';
+var isNull=null;
+console.log(Boolean(isNull))
+var c= false;
+console.log(Boolean(c))
+
+var nan= NaN;
+console.log(Boolean(a))
